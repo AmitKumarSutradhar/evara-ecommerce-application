ef7a369c70831765107d617c8e6259c26488da9d
Amit Kumar Sutradhar <dev.amitkumarsutradhar@gmail.com>
2024-11-10 00:11:04 +0600

    Mastering home page

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\EvaraController;
 
 /*
 |--------------------------------------------------------------------------
@@ -13,6 +14,4 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/', [EvaraController::class,'index'])->name('home');
